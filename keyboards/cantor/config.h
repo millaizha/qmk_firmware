ab1918c5eb6056f27c80cf455c24e261bfa79d7e
Mill <39644596+millaizha@users.noreply.github.com>
2023-02-22 12:06:04 +0800

    Update cantor remix keymap

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -22,15 +22,8 @@
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
-/* Top left key on left half */
-#define BOOTMAGIC_LITE_ROW    0
-#define BOOTMAGIC_LITE_COLUMN 0
-/* Top right key on right half */
-#define BOOTMAGIC_LITE_ROW_RIGHT    4
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
-
 // Tap hold configurations
-#define TAPPING_TERM 270
+#define TAPPING_TERM 350
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 
--- keyboards/cantor/keymaps/millaizha/keymap.c
+++ keyboards/cantor/keymaps/millaizha/keymap.c
@@ -5,6 +5,18 @@
 
 #include QMK_KEYBOARD_H
 
+// Layers
+#define _LAYER0 0
+#define _LAYER1 1
+#define _LAYER2 2
+#define _LAYER3 3
+#define _LAYER4 4
+#define _LAYER5 5
+#define _LAYER6 6
+#define _LAYER7 7
+#define _LAYER8 8
+#define _LAYER9 9
+
 // Left-hand home row mods
 #define GUI_A LGUI_T(KC_A)
 #define ALT_R LALT_T(KC_R)
@@ -23,7 +35,7 @@
 #define TAB_5 LT(5,KC_TAB)
 #define ENT_3 LT(3,KC_ENT)
 #define SPC_1 LT(1,KC_SPC)
-#define SPC_8 LT(9,KC_SPC)
+#define SPC_7 LT(8,KC_SPC)
 #define DEL_2 LT(2,KC_DEL)
 
 // Modifier key shortcuts
@@ -32,34 +44,52 @@
 #define SETTINGS LGUI(KC_I)
 
 // Multi-tap function tap dance
-enum multi_tap_dance {
+enum multi_tap_dance 
+{
     TD_SINGLE_TAP,
     TD_SINGLE_HOLD,
     TD_DOUBLE_TAP,
     TD_DOUBLE_HOLD,
     TD_DOUBLE_SINGLE_TAP,
     TD_TRIPLE_TAP,
-    TD_TRIPLE_HOLD
+    TD_TRIPLE_HOLD,
 };
 
-typedef struct {
+typedef struct
+{
     bool is_press_action;
     int state;
 } td_tap_t;
 
+enum custom_keycodes 
+{
+    LAYER0 = SAFE_RANGE,
+    LAYER1,
+    LAYER2,
+    LAYER3,
+    LAYER4,
+    LAYER5,
+    LAYER6,
+    LAYER7,
+    LAYER8,
+    LAYER9,
+};
+
 // Tap dance and custom keycodes declarations
-enum custom_keycodes {
+enum 
+{
     COPY_C,
     CUT_X,
     PASTE_V,
+    BSPC_T_DANCE,
     LOL_T_DANCE,
-    VALO_T_DANCE,
     GAME_CHAT,
     GAME
 };
 
 // Multi tap dance functions
-int cur_dance(qk_tap_dance_state_t *state) {
+int cur_dance(qk_tap_dance_state_t *state) 
+{
     if (state->count == 1) {
         if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
         else return TD_SINGLE_HOLD;
@@ -71,96 +101,75 @@ int cur_dance(qk_tap_dance_state_t *state) {
         if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
         else return TD_TRIPLE_HOLD;
     } else return 7;
-}
+};
 
 // Tap dance config for game team/all chat
-void td_game_chat_finished(qk_tap_dance_state_t *state, void *user_data) {
-    layer_on(8);
+void td_game_chat_finished(qk_tap_dance_state_t *state, void *user_data) 
+{
+    layer_on(7);
     if (state->count == 1) {
         register_code16(KC_ENT);
     } else {
         register_code16(LSFT(KC_ENT));
     }
-}
+};
 
-void td_game_chat_reset(qk_tap_dance_state_t *state, void *user_data) {
+void td_game_chat_reset(qk_tap_dance_state_t *state, void *user_data) 
+{
     if (state->count == 1) {
         unregister_code16(KC_ENT);
     } else {
         unregister_code16(LSFT(KC_ENT));
     }
-}
+};
 
 // Key event after chatting in game
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) 
+{
     switch (keycode) {
         case GAME:
             if (record->event.pressed) {
                 register_code16(KC_ENT);
-                layer_off(8);
+                layer_off(7);
                 unregister_code16(KC_ENT);
             }
             break;
     }
     return true;
-}
+};
 
 // LOL quad tap dance
-static td_tap_t lol_tap_state = {
+static td_tap_t lol_tap_state = 
+{
     .is_press_action = true,
     .state = 0
 };
 
-void td_lol_finished(qk_tap_dance_state_t *state, void *user_data) {
+void td_lol_finished(qk_tap_dance_state_t *state, void *user_data) 
+{
     lol_tap_state.state = cur_dance(state);
     switch (lol_tap_state.state) {
-        case TD_SINGLE_TAP: register_code(KC_GRV); break;
+        case TD_SINGLE_TAP: register_code(KC_P); break;
         case TD_SINGLE_HOLD: register_code(KC_TAB); break;
         case TD_DOUBLE_TAP: register_code(KC_ESC); break;
-        case TD_DOUBLE_HOLD: layer_on(10);
+        case TD_DOUBLE_HOLD: layer_on(9);
     }
-}
-
-void td_lol_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (lol_tap_state.state) {
-        case TD_SINGLE_TAP: unregister_code(KC_GRV); break;
-        case TD_SINGLE_HOLD: unregister_code(KC_TAB); break;
-        case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
-        case TD_DOUBLE_HOLD: layer_off(10);
-    }
-    lol_tap_state.state = 0;
-}
-
-// Valorant quad tap dance
-static td_tap_t valo_tap_state = {
-    .is_press_action = true,
-    .state = 0
 };
 
-void td_valo_finished(qk_tap_dance_state_t *state, void *user_data) {
-    valo_tap_state.state = cur_dance(state);
-    switch (valo_tap_state.state) {
-        case TD_SINGLE_TAP: register_code(KC_B); break;
-        case TD_SINGLE_HOLD: register_code(KC_LALT); break;
-        case TD_DOUBLE_TAP: register_code(KC_T); break;
-        case TD_DOUBLE_HOLD: layer_on(10); break;
-        case TD_TRIPLE_TAP: register_code(KC_ESC);
+void td_lol_reset(qk_tap_dance_state_t *state, void *user_data) 
+{
+    switch (lol_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code(KC_P); break;
+        case TD_SINGLE_HOLD: unregister_code(KC_TAB); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_ESC); break;
+        case TD_DOUBLE_HOLD: layer_off(9);
     }
-}
-
-void td_valo_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (valo_tap_state.state) {
-        case TD_SINGLE_TAP: unregister_code(KC_B); break;
-        case TD_SINGLE_HOLD: unregister_code(KC_LALT); break;
-        case TD_DOUBLE_TAP: unregister_code(KC_T); break;
-        case TD_DOUBLE_HOLD: layer_off(10); break;
-        case TD_TRIPLE_TAP: unregister_code(KC_ESC);
-    }
-    valo_tap_state.state = 0;
-}
+    lol_tap_state.state = 0;
+};
 
 // Tap dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
+qk_tap_dance_action_t tap_dance_actions[] = 
+{
     // Tap once for C, twice for Copy
     [COPY_C] = ACTION_TAP_DANCE_DOUBLE(KC_C, LCTL(KC_C)),
     // Tap once for X, twice for Cut
@@ -171,24 +180,23 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [GAME_CHAT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_game_chat_finished, td_game_chat_reset),
     // Tap once for Grave, twice for Escape. Hold for Tab, tap + hold for MO(10)
     [LOL_T_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_lol_finished, td_lol_reset),
-    // Tap once for B, twice for T, thrice for Escape. Hold for Alt, tap + hold for MO(10)
-    [VALO_T_DANCE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_valo_finished, td_valo_reset)
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    
-    [0] = LAYOUT_split_3x6_3(
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = 
+{
+
+  [_LAYER0] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,  KC_QUOT,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
              KC_NO,   GUI_A,   ALT_R,   CTL_S,  SHFT_T,   KC_G,                         KC_M,    SFT_N,   CTL_E,   ALT_I,  GUI_O,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            KC_NO,    KC_Z, TD(CUT_X), TD(COPY_C), KC_D, TD(PASTE_V),                   KC_K,    KC_H,  KC_COMM,  KC_DOT, KC_SLSH,  KC_NO, 
+             KC_NO,   KC_Z, TD(CUT_X), TD(COPY_C), KC_D, TD(PASTE_V),                   KC_K,    KC_H,  KC_COMM,  KC_DOT, KC_SLSH,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                                  ESC_4, BSPC_3, TAB_5,          ENT_3, SPC_1, DEL_2
+                                                  ESC_4, BSPC_3, TAB_5,         ENT_3, SPC_1, DEL_2
                                             //`--------------------------'  `--------------------------'
     ),
-    [1] = LAYOUT_split_3x6_3(
+    [_LAYER1] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO, KC_LBRC,   KC_7,    KC_8,    KC_9,  KC_RBRC,                        KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -200,20 +208,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             //`--------------------------'  `--------------------------'
 
     ),
-    [2] = LAYOUT_split_3x6_3(
+    [_LAYER2] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-             KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                      KC_BTN2, KC_RSFT, KC_RCTL, KC_RALT, KC_RSFT,  KC_NO, 
+             KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                      KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                      KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_NO, 
+             KC_NO, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                      KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO, KC_TILD, KC_EXLM,  KC_AT,  KC_HASH, KC_PIPE,                      KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  KC_NO, 
+             KC_NO, KC_TILD, KC_EXLM,  KC_AT,  KC_HASH, KC_PIPE,                      KC_BTN3, KC_BTN4, KC_BTN5, KC_WH_U, KC_WH_R,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                                KC_LPRN, KC_RPRN, KC_UNDS,       KC_NO, KC_NO, KC_TRNS
                                             //`--------------------------'  `--------------------------'
     ),
-    [3] = LAYOUT_split_3x6_3(
+    [_LAYER3] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-             KC_NO, QK_BOOT, KC_MPRV, KC_MPLY, KC_MNXT, LOCK_PC,                       KC_NO,  KC_RALT,  KC_UP,  KC_TAB,  KC_CALC,  KC_NO, 
+             KC_NO, KC_CALC, KC_MPRV, KC_MPLY, KC_MNXT, LOCK_PC,                       KC_NO,  KC_RALT,  KC_UP,  KC_TAB,  KC_FIND,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
              KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, SNIP_PC,                      KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, SETTINGS, KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -222,29 +230,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_NO, KC_TRNS, KC_NO,         KC_TRNS, KC_NO, KC_NO
                                             //`--------------------------'  `--------------------------'
     ),
-    [4] = LAYOUT_split_3x6_3(
+    [_LAYER4] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-             KC_NO, KC_F12,   KC_F7,   KC_F8,   KC_F9,  KC_PSCR,                      KC_BTN2, KC_RSFT, KC_RCTL, KC_RALT, KC_ENT,   KC_NO, 
+             KC_NO, KC_F12,   KC_F7,   KC_F8,   KC_F9,  KC_PSCR,                      KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO, KC_F11,   KC_F4,   KC_F5,   KC_F6,  KC_SCRL,                      KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_NO, 
+             KC_NO, KC_F11,   KC_F4,   KC_F5,   KC_F6,  KC_SCRL,                      KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO, KC_F10,   KC_F1,   KC_F2,   KC_F3,  KC_PAUS,                      KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,  KC_NO, 
+             KC_NO, KC_F10,   KC_F1,   KC_F2,   KC_F3,  KC_PAUS,                      KC_BTN3, KC_BTN4, KC_BTN5, KC_WH_U, KC_WH_R,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                                  KC_TRNS, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO
                                             //`--------------------------'  `--------------------------'
     ),
-    [5] = LAYOUT_split_3x6_3(
+    [_LAYER5] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO,  KC_NO,   KC_NO,   TO(7),   TO(6),   KC_NO,                        KC_NO,   KC_LEFT,KC_DOWN, KC_RGHT,  KC_NO,   KC_NO,
+             KC_NO,  KC_NO,   KC_NO,   KC_NO,   TO(6),   KC_NO,                        KC_NO,   KC_LEFT,KC_DOWN, KC_RGHT,  KC_NO,   KC_NO,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
              KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                                   KC_NO, KC_NO, KC_TRNS,        KC_NO, KC_NO, KC_NO
                                             //`--------------------------'  `--------------------------'
     ),
-    [6] = LAYOUT_split_3x6_3(
+    [_LAYER6] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO,   KC_1,   KC_2,     KC_3,    KC_4,    KC_5,                        KC_NO,  KC_RALT, KC_TAB,   KC_NO,   KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -255,29 +263,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                          TD(LOL_T_DANCE), KC_SPC, KC_LCTL, TD(GAME_CHAT), LALT(KC_Z), TO(0)
                                             //`--------------------------'  `--------------------------'
     ),
-    [7] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-             KC_NO,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,                        KC_NO,  KC_RALT, KC_TAB,   KC_NO,   KC_NO,   KC_NO,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO, KC_LSFT,   KC_A,    KC_S,    KC_D,    KC_F,                        KC_NO,  KC_MPRV, KC_MPLY, KC_MNXT,  KC_NO,   KC_NO,
-        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO, KC_LCTL,   KC_4,    KC_X,    KC_C,    KC_V,                        KC_NO,  KC_VOLD, KC_VOLU,  KC_NO,   KC_NO,   KC_NO,  
-        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          TD(VALO_T_DANCE), KC_SPC, KC_G, TD(GAME_CHAT), LALT(KC_Z), TO(0)
-                                            //`--------------------------'  `--------------------------'
-    ),
-    [8] = LAYOUT_split_3x6_3(
+    [_LAYER7] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,  KC_QUOT,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-             KC_NO,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O,   KC_NO, 
+             KC_NO,   GUI_A,   ALT_R,   CTL_S,  SHFT_T,   KC_G,                         KC_M,    SFT_N,   CTL_E,   ALT_I,  GUI_O,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
              KC_NO,   KC_Z,    KC_X, TD(COPY_C), KC_D, TD(PASTE_V),                     KC_K,    KC_H,  KC_COMM, KC_DOT,  KC_SLSH,  KC_NO, 
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                                KC_LSFT, KC_BSPC, KC_TAB,       GAME, SPC_8, KC_CAPS
+                                                KC_LSFT, KC_BSPC, KC_TAB,       GAME, SPC_7, KC_CAPS
                                             //`--------------------------'  `--------------------------'
     ),
-    [9] = LAYOUT_split_3x6_3(
+    [_LAYER8] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO, KC_LBRC,   KC_7,    KC_8,    KC_9,  KC_RBRC,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -288,7 +285,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_DOT, KC_0, KC_MINS,        KC_NO, KC_TRNS, KC_NO
                                             //`--------------------------'  `--------------------------'
     ),
-    [10] = LAYOUT_split_3x6_3(
+    [_LAYER9] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
