e5a95b7a27ecacbeb0961c773896a7853fa00b0c
Mill <millaizhavalencia@gmail.com>
2024-04-09 00:59:31 +0800

    remove game keymap

--- keyboards/reviung/reviung34/keymaps/millaizha/keymap.c
+++ keyboards/reviung/reviung34/keymaps/millaizha/keymap.c
@@ -12,7 +12,6 @@ char wpm_str[10];
 #define QWERTY 3
 #define WINMISC 4
 #define QWRTARR 5
-#define WINGAME 6)
 
 enum custom_keycodes {
     N_TILDE = SAFE_RANGE,
@@ -35,8 +34,7 @@ qk_tap_dance_action_t tap_dance_actions[] =
     [COPY_Q] = ACTION_TAP_DANCE_DOUBLE(KC_Q, LCTL(KC_C)),
     [CUT_X] = ACTION_TAP_DANCE_DOUBLE(KC_X, LCTL(KC_X)),
     [PASTE_V] = ACTION_TAP_DANCE_DOUBLE(KC_V, LCTL(KC_V)),
-    [UNDO_Z] = ACTION_TAP_DANCE_DOUBLE(KC_Z, LCTL(KC_Z)),
-    [CLMK] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, TO(0)),
+    [UNDO_Z] = ACTION_TAP_DANCE_DOUBLE(KC_Z, LCTL(KC_Z))
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -94,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------------+--------------+--------------+--------------+--------------|                    |--------------+--------------+--------------+--------------+--------------|
        LGUI(KC_LEFT),    KC_MPRV,       KC_MPLY,       KC_MNXT,    LGUI(KC_RGHT),                          KC_BRIU,      KC_MS_L,       KC_MS_D,       KC_MS_U,       KC_MS_R,  
     //|--------------+--------------+--------------+--------------+--------------|                    |--------------+--------------+--------------+--------------+--------------|
-         LGUI(KC_E),    LGUI(KC_I),    LGUI(KC_L),    RCS(KC_S),     LGUI(KC_V),                           KC_BRID,      KC_WH_L,       KC_WH_D,       KC_WH_U,       KC_WH_R,  
+         LGUI(KC_E),    LGUI(KC_I),    LGUI(KC_L),   SGUI(KC_S),     LGUI(KC_V),                           KC_BRID,      KC_WH_L,       KC_WH_D,       KC_WH_U,       KC_WH_R,  
     //`--------------+--------------+--------------+--------------+--------------'                    `--------------+--------------+--------------+--------------+--------------'
                                                        KC_TRNS,        KC_NO,                               KC_NO,       KC_TRNS 
                                                  //`--------------+--------------'                    `--------------+--------------'
@@ -108,17 +106,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC,       TO(0),        KC_VOLD,       KC_VOLU,       KC_MUTE,                              KC_CAPS,      KC_HOME,       KC_PGDN,      KC_PGUP,       KC_END, 
     //`--------------+--------------+--------------+--------------+--------------'                    `--------------+--------------+--------------+--------------+--------------'
                                                      LT(4,KC_DEL),    KC_INS,                                KC_NO,       KC_TRNS 
-                                                 //`--------------+--------------'                    `--------------+--------------'
-    ),
-[WINGAME] = LAYOUT_reviung34(
-    //,--------------------------------------------------------------------------.                    ,--------------------------------------------------------------------------.
-            KC_TAB,        KC_Q,          KC_W,         KC_E,           KC_R,                               KC_T,         KC_Y,          KC_U,          KC_I,          KC_O, 
-    //|--------------+--------------+--------------+--------------+--------------|                    |--------------+--------------+--------------+--------------+--------------|
-            KC_LSFT,       KC_A,          KC_S,         KC_D,           KC_F,                               KC_G,         KC_H,          KC_J,          KC_K,          KC_L,  
-    //|--------------+--------------+--------------+--------------+--------------|                    |--------------+--------------+--------------+--------------+--------------|
-            KC_LCTL,       KC_Z,          KC_X,         KC_C,           KC_V,                               KC_B,         KC_N,          KC_M,          KC_P,         TD(CLMK),  
-    //`--------------+--------------+--------------+--------------+--------------'                    `--------------+--------------+--------------+--------------+--------------'
-                                                       KC_SPC,        TD(CLMK),                             KC_1,         KC_2 
                                                  //`--------------+--------------'                    `--------------+--------------'
     )
 };
